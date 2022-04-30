c68b3f06207ee313ccd92e585332ae9c89f54e94
Emerson <51987534+Emerson89@users.noreply.github.com>
2022-04-30 14:30:27 -0300

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install httpd > /var/log/installapache.out 2>&1
-service httpd restart > /var/log/restartapache.out 2>&1
+sudo yum install httpd > /var/log/installapache.out 2>&1
+sudo service httpd restart > /var/log/restartapache.out 2>&1
